1f45dba8aad4df2e93f1d92f8fbe750f2d1c8ba5
Kostia Savchenko <k.savchenko.dev@gmail.com>
2025-03-13 12:05:45 +0300

    implement MarvelService for fetching character data; update main entry point to log character names

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./style/style.scss";
+import MarvelService from './services/MarvelService';
 import App from './components/app/App'
+
+const marvelService = new MarvelService();
+
+marvelService.getAllCharacters().then((res) => {
+  res.data.results.forEach(item => console.log(item.name));
+}
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
