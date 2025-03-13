7aac18e109128dc504da778461d894a3e98551d8
Kostia Savchenko <k.savchenko.dev@gmail.com>
2025-03-13 12:50:37 +0300

    refactor RandomChar component to use MarvelService for dynamic character fetching; update MarvelService methods for improved data handling

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,7 @@ import "./style/style.scss";
 import MarvelService from './services/MarvelService';
 import App from './components/app/App'
 
-const marvelService = new MarvelService();
 
-marvelService.getAllCharacters().then((res) => {
-  res.data.results.forEach(item => console.log(item.name));
-}
-);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
